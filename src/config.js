a37c6e1c26bcdf970970367b3c16dfa0f6261663
Jesse Tran <jessetran03@gmail.com>
2021-04-26 14:40:44 -0500

    Updated API key

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   RATING_ENDPOINT: 'http://localhost:8000/api/ratings',
   API_ENDPOINT: 'http://localhost:8000/api/businesses',
   //API_ENDPOINT: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses',
-  API_KEY: 'PuS1R-v_MKSC8Quur5U7ynpLI-4NJLo8y-aEOUo00TzHNI37Ze3JrlziaTy_Cxiq6NA-svPl17mRaF2aW5aTFytcAOYLWRZesEgxiNWnJqakiVXTTFdjsOsjIPGCYHYx'
+  API_KEY: process.env.API_KEY
 }
 
 export default config;
